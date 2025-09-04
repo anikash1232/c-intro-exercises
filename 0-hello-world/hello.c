c1e0b8af846c1df412f230255c6e326a2e170040
anikash1232 <anirudh.vpk@gmail.com>
2025-09-04 16:10:53 +0000

    Add Part 0: hello.c (PID header, prints, EXIT_SUCCESS)

---
+++ 0-hello-world/hello.c
@@ -0,0 +1,10 @@
+// PID: 730761368
+// I pledge the COMP211 honor code.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("hello, world\n");
+    printf("welcome to c!\n");
+    return EXIT_SUCCESS;
+}
